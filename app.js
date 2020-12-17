7daaeb681076a3c24c6d6184f45a52b2e384eac5
“[firstname <jacbraq@dunwoody.edu>
2020-12-17 10:03:36 -0600

    adjusted the display of menu items to appear in the content section. but menu items  populates as blank objects

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var createUsersRouter = require('./routes/createUsers');
 var accountRouter = require('./routes/account');
 var searchRouter = require('./routes/search');
+var searchMenuRouter = require('./routes/searchMenu');
 
 
 var app = express();
@@ -26,6 +27,8 @@ app.use('/', indexRouter);
 app.use(createUsersRouter);
 app.use(accountRouter);
 app.use('/search',searchRouter);
+// added searchMenu
+app.use('/searchMenu',searchMenuRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var restaurantData = require('../database/restaurant.json');
+var menuData = require('../database/restaurant.json');
 
-/* GET home page. */
+/* GET search page. */
 router.get('/', function(req, res, next) {
 var objectArray = [];
 
@@ -12,5 +13,17 @@ var objectArray = [];
 
   res.render('search', { restaurantList : objectArray });
 });
+
+// Get menu items after selecting restaurant
+router.get('/', function(req, res, next) {
+  var menuArray = [];
+  
+    for(var x = 0; x < menuData.dataroot.TB_Restaurant.TB_Menu.length; x++){
+        menuArray.push(menuData.dataroot.TB_Restaurant[x].TB_Menu[x].food_item);
+    }
+  
+  res.render('searchMenu', { menuList : menuArray });
+});
+
 
 module.exports = router;
